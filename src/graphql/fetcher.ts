6f94f4e3ee08fab4486b2daf5155b0bdce7f528a
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-06 23:00:15 +0900

    add:useSWR追加、apolloサーバーとの接続

---
+++ src/graphql/fetcher.ts
@@ -0,0 +1,10 @@
+export const normalFetcher = (query:String) =>
+  fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then((json) => json.data);
--- src/pages/iron-duke/index.tsx
+++ src/pages/iron-duke/index.tsx
@@ -2,8 +2,18 @@ import * as React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import useSWR from 'swr';
+import { normalFetcher } from '../../graphql/fetcher';
 
+const query = `query{
+  sampleHello
+}`;
 export default function Index() {
+  const { data, error } = useSWR(query, normalFetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
