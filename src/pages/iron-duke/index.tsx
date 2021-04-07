327fb378bfcaba552c19db9f7062e340867abb74
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-07 20:56:47 +0900

    add:useSWRの型定義とtodolist一個取得してみる

--- src/pages/iron-duke/index.tsx
+++ src/pages/iron-duke/index.tsx
@@ -5,21 +5,43 @@ import Box from '@material-ui/core/Box';
 import useSWR from 'swr';
 import { normalFetcher } from '../../graphql/fetcher';
 
+type TodoListEntity = {
+  getToDolist: {
+    id: Number;
+    firstName: String;
+    lastName: String;
+    todo: String;
+    createdAt: String;
+  };
+};
+
 const query = `query{
-  sampleHello
+  getToDolist{
+    id
+    firstName
+    lastName
+    todo
+    createdAt
+  }
 }`;
+
 export default function Index() {
-  const { data, error } = useSWR(query, normalFetcher);
+  const { data, error } = useSWR<TodoListEntity>(query, normalFetcher);
 
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
-
+  console.log(data);
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           To Do Listd
         </Typography>
+        <div>{data.getToDolist.id}</div>
+        <div>{data.getToDolist.firstName}</div>
+        <div>{data.getToDolist.lastName}</div>
+        <div>{data.getToDolist.todo}</div>
+        <div>{data.getToDolist.createdAt}</div>
       </Box>
     </Container>
   );
