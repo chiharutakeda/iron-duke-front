f1b919e07936c18d82eb6635fe6fc521407f4207
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-07 22:25:29 +0900

    add:コメント追加

--- src/graphql/fetcher.ts
+++ src/graphql/fetcher.ts
@@ -1,9 +1,13 @@
 import { request } from 'graphql-request';
 
+/*
+gqlfetcherとnormalFetcherは同じ機能。methodとかheadersとか書かなくてよくなるしres.jsonとかも
+内包してもらえて短く書ける
+*/
 export const API_ENDPOINT = 'http://localhost:4000/graphql';
 
 //graphql-request使えばかなりシンプルになる
-export const gqlfetcher = (query:string) => request('/api/graphql', query);
+export const gqlfetcher = (query: string) => request('/api/graphql', query);
 
 //ただのfetchでもできるけどちょっと長い
 export const normalFetcher = (query: String) =>
