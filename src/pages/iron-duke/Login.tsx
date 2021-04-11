9cdf15ef68f8d4bb7448c575c1753430402cd536
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-11 14:45:30 +0900

    fix:不要import削除

--- src/pages/iron-duke/Login.tsx
+++ src/pages/iron-duke/Login.tsx
@@ -1,5 +1,4 @@
 import React, { useReducer } from 'react';
-import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
