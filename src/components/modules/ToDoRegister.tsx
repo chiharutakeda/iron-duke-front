5ca8b75541da38c9f66dd188c123a73e3f4323b6
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-18 17:48:51 +0900

    add:入力して登録できるようにした

--- src/components/modules/ToDoRegister.tsx
+++ src/components/modules/ToDoRegister.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { gqlfetcher } from '@/fetch/fetcher';
+import { gql } from 'graphql-request';
+import { ToDoRegistReducer, initialToDoRegist } from '@/reducer/toDoRegistReducer';
 
 //この変数に与える型はサーバー側の型をschemaを参照する。
-const registToDolist_M = `
+const registToDolist_M = gql`
   mutation RegistToDo($todo: TodoListInputType!) {
     RegistToDo(todo: $todo) {
       id
@@ -12,28 +14,48 @@ const registToDolist_M = `
       lastName
       todo
     }
-  },
+  }
 `;
 
-//変数の渡し方慣れないと難しい
-const registToDolist_V = {
-  todo: {
-    firstName: 'takeda',
-    lastName: 'chiahru',
-    todo: 'hello',
-  },
-};
-
 const ToDoRegister = () => {
+  const [state, dispatch] = useReducer(ToDoRegistReducer, initialToDoRegist);
   return (
     <form>
-      <TextField id="filled-basic" label="FIRST_NANE" fullWidth />
-      <TextField id="filled-basic" label="LAST_NAME" fullWidth />
-      <TextField id="filled-basic" label="TODO" fullWidth multiline={true} rows="3" />
+      <TextField
+        id="filled-basic"
+        label="FIRST_NANE"
+        fullWidth
+        value={state.todo.firstName}
+        onChange={(e) => {
+          dispatch({ type: 'CHANGE_FIRST_NAME', payload: e.target.value });
+        }}
+      />
+      <TextField
+        id="filled-basic"
+        label="LAST_NAME"
+        fullWidth
+        value={state.todo.lastName}
+        onChange={(e) => {
+          dispatch({ type: 'CHANGE_LAST_NAME', payload: e.target.value });
+        }}
+      />
+      <TextField
+        id="filled-basic"
+        label="TODO"
+        fullWidth
+        multiline={true}
+        rows="3"
+        value={state.todo.todo}
+        onChange={(e) => {
+          dispatch({ type: 'CHANGE_TODO', payload: e.target.value });
+        }}
+      />
       <Button
         variant="contained"
         color="primary"
         onClick={() => {
+          //stateそのまま渡せる形にしてある
+          const registToDolist_V = state;
           gqlfetcher(registToDolist_M, registToDolist_V);
         }}
       >
---
+++ src/reducer/toDoRegistReducer.ts
@@ -0,0 +1,35 @@
+export const initialToDoRegist: TypeToDoRegistState = {
+  //入力値をそのまま渡せるような形にする。
+  //これ重要テクな気がする。
+  todo: {
+    firstName: '',
+    lastName: '',
+    todo: '',
+  },
+};
+
+type TypeToDoRegistState = {
+  todo: {
+    firstName: string;
+    lastName: string;
+    todo: string;
+  };
+};
+
+type TypeToDoRegist =
+  | { type: 'CHANGE_FIRST_NAME'; payload: string }
+  | { type: 'CHANGE_LAST_NAME'; payload: string }
+  | { type: 'CHANGE_TODO'; payload: string };
+
+export const ToDoRegistReducer = (state: TypeToDoRegistState, action: TypeToDoRegist): TypeToDoRegistState => {
+  switch (action.type) {
+    case 'CHANGE_FIRST_NAME':
+      return { ...state, todo: { ...state.todo, firstName: action.payload } };
+    case 'CHANGE_LAST_NAME':
+      return { ...state, todo: { ...state.todo, lastName: action.payload } };
+    case 'CHANGE_TODO':
+      return { ...state, todo: { ...state.todo, todo: action.payload } };
+    default:
+      return { ...state };
+  }
+};
