8833b6f9fb4e16a2dc3b82004377611865022f39
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-18 16:17:39 +0900

    add:mutation変数渡せるようにする

--- src/components/modules/ToDoRegister.tsx
+++ src/components/modules/ToDoRegister.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { gqlfetcher } from '@/fetch/fetcher';
-//変数なしmutate
-const registToDolist_M = `mutation{
-  RegistToDo(todo:{firstName:"takeday",lastName:"chiharu",todo:"nextstage"}){
-    id
-    firstName
-    lastName
-    todo
-  }
-}`;
+
+//この変数に与える型はサーバー側の型をschemaを参照する。
+const registToDolist_M = `
+  mutation RegistToDo($todo: TodoListInputType!) {
+    RegistToDo(todo: $todo) {
+      id
+      firstName
+      lastName
+      todo
+    }
+  },
+`;
+
+//変数の渡し方慣れないと難しい
+const registToDolist_V = {
+  todo: {
+    firstName: 'takeda',
+    lastName: 'chiahru',
+    todo: 'hello',
+  },
+};
+
 const ToDoRegister = () => {
   return (
     <form>
       <TextField id="filled-basic" label="FIRST_NANE" fullWidth />
-      <TextField id="filled-basic" label="LAST_NAME"  fullWidth />
-      <TextField id="filled-basic" label="TODO"  fullWidth multiline={true} rows="3"/>
+      <TextField id="filled-basic" label="LAST_NAME" fullWidth />
+      <TextField id="filled-basic" label="TODO" fullWidth multiline={true} rows="3" />
       <Button
         variant="contained"
         color="primary"
         onClick={() => {
-          gqlfetcher(registToDolist_M);
+          gqlfetcher(registToDolist_M, registToDolist_V);
         }}
       >
         Regist
--- src/pages/iron-duke/index.tsx
+++ src/pages/iron-duke/index.tsx
@@ -4,48 +4,14 @@ import AllToDoList from '@/components/modules/AllToDoList'
 import ToDoRegister from '@/components/modules/ToDoRegister'
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import useSWR from 'swr';
-import { gqlfetcher } from '@/fetch/fetcher';
-// import { normalFetcher } from '@/fetch/fetcher';
-
-type TodoListEntity = {
-  getToDolist: {
-    id: number;
-    firstName: String;
-    lastName: String;
-    todo: String;
-    createdAt: String;
-  };
-};
-
-//変数なしquery
-const getToDolist_Q = `query{
-  getToDolist{
-    id
-    firstName
-    lastName
-    todo
-    createdAt
-  }
-}`;
 
 export default function Index() {
-  // const { data, error } = useSWR<TodoListEntity>(getToDolist_Q, normalFetcher); //普通のfetcherバージョン
-  const { data, error } = useSWR<TodoListEntity>(getToDolist_Q, gqlfetcher);
-
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Regist To Do
         </Typography>
-        <div>{data.getToDolist.id}</div>
-        <div>{data.getToDolist.firstName}</div>
-        <div>{data.getToDolist.lastName}</div>
-        <div>{data.getToDolist.todo}</div>
-        <div>{data.getToDolist.createdAt}</div>
       </Box>
       <ToDoRegister />
       <AllToDoList />
