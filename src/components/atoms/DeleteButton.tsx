aeaba7e610af9cdb9608c0e4c21fda2c70fb3bba
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-23 21:31:06 +0900

    add:削除ボタン追加

--- src/components/atoms/DeleteButton.tsx
+++ src/components/atoms/DeleteButton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { gql } from 'graphql-request';
 import { gqlfetcher } from '@/fetch/fetcher';
+import { DeleteToDOVariables } from '@/generated/DeleteToDO';
 
 //この変数に与える型はサーバー側の型をschemaを参照する。
 const deleteToDolist_M = gql`
@@ -20,7 +21,7 @@ const DeleteButton = (deleteId: { ID: number }) => {
       variant="contained"
       color="inherit"
       onClick={() => {
-        const deleteToDolist_V = { deleteTODO: deleteId };
+        const deleteToDolist_V: DeleteToDOVariables = { deleteTODO: deleteId };
         gqlfetcher(deleteToDolist_M, deleteToDolist_V);
       }}
     >
