4e041042eda69b12130b134041b6e94e803eaab6
Takeda Chiharu <takeda123pc@gmail.com>
2021-05-08 22:15:34 +0900

    add:cssモジュールのテストの設定

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
-//jestの設定ファイル
-//moduleNameMapperでaliasのパス設定できる
-//configの呼び方が複雑（jest.config.jsからtsconfig.jest.jsonを参照しそこからtsconfig.jsonを参照している）
-//ある設定ファイルの設定をそのまま使うようなことが案外多い
 module.exports = {
-  moduleNameMapper: { '@/(.+)': '<rootDir>/src/$1' },
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '@/(.+)': '<rootDir>/src/$1',
+  },
 };
--- src/components/atoms/DeleteButton.tsx
+++ src/components/atoms/DeleteButton.tsx
@@ -1,23 +1,12 @@
 import { gqlfetcher } from '@/fetch/fetcher';
 import { DeleteToDOVariables } from '@/generated/DeleteToDO';
 import { deleteToDolist_M } from '@/gql/gql';
-
-const style = {
-  margin: '3px 0px',
-  height: '2rem',
-  width: 'auto',
-  color: 'black',
-  background: '#CC0000',
-  borderRadius: '5px',
-  borderStyle: 'none',
-  boxShadow: '1px 1px 4px',
-  cursor: 'pointer',
-};
+import styles from './styles/Deletebutton.module.css';
 
 const DeleteButton = (deleteId: { ID: number }) => {
   return (
     <button
-      style={style}
+      className={styles.DeleteButton}
       onClick={() => {
         const deleteToDolist_V: DeleteToDOVariables = { deleteTODO: deleteId };
         gqlfetcher(deleteToDolist_M, deleteToDolist_V);
