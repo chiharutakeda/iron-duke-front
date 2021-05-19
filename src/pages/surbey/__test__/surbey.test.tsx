96d6ee6aa1da39879b53ee0ccd3f8763beb98473
Takeda Chiharu <takeda123pc@gmail.com>
2021-05-19 22:24:07 +0900

    fix

--- src/pages/surbey/__test__/surbey.test.tsx
+++ src/pages/surbey/__test__/surbey.test.tsx
@@ -2,6 +2,7 @@ import {getPidFromPath,switchPage} from '@/pages/surbey/[pid]'
 import SurbeyFirst from '@/components/organisms/survey/SurbeyFirst'
 import SurbeySecond from '@/components/organisms/survey/SurbeySecond'
 import SurbeyThird from '@/components/organisms/survey/SurbeyThird'
+import SurbeyFinish from '@/components/organisms/survey/SurbeyFinish'
 test('getPidFromPath', () => {
   expect(getPidFromPath('aaa/bbb/ccc')).toBe("ccc");
 });
@@ -9,5 +10,6 @@ test('switchPage', () => {
   expect(switchPage('SurbeyFirst')).toMatchObject(<SurbeyFirst />);
   expect(switchPage('SurbeySecond')).toMatchObject(<SurbeySecond />);
   expect(switchPage('SurbeyThird')).toMatchObject(<SurbeyThird />);
+  expect(switchPage('SurbeyFinish')).toMatchObject(<SurbeyFinish />);
 });
 
